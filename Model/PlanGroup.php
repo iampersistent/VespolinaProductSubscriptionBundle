58ee5bdaed41f40a158fa983502934f38830528d
IamPersistent <develop@zestic.com>
2012-03-07 04:23:43 -0800

    initial commit

---
+++ Model/PlanGroup.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: iampersistent
+ * Date: 3/7/12
+ * Time: 2:51 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class PlanGroup
+{
+
+}
